526e3df987c559d078ecc9ff8e05b9f9fb11d5b9
Neto Chaves <netobac1@gmail.com>
2019-10-30 14:23:10 -0300

    feat: add drag and drop to project board

--- app/assets/javascripts/components/Columns/ColumnItem.js
+++ app/assets/javascripts/components/Columns/ColumnItem.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Droppable } from 'react-beautiful-dnd'
 
-const Column = ({ title, children, renderAction, onClose }) => (
-  <div className="Column">
-    <div className="Column__header">
-      <h3 className="Column__name">{title}</h3>
-      <div className="Column__actions">
-        {renderAction()}
-        <button type="button" className="Column__btn-close" onClick={onClose}>
-          <i className="mi md-light md-16">close</i>
-        </button>
+const Column = ({ title, children, renderAction, onClose, columnId }) => (
+  <Droppable droppableId={columnId} isDropDisabled={columnId === 'done'}>
+    {provided => (
+      <div className="Column" ref={provided.innerRef} {...provided.droppableProps}>
+        <div className="Column__header">
+          <h3 className="Column__name">{title}</h3>
+          <div className="Column__actions">
+            {renderAction()}
+            <button type="button" className="Column__btn-close" onClick={onClose}>
+              <i className="mi md-light md-16">close</i>
+            </button>
+          </div>
+        </div>
+        <div className="Column__body">
+          {children}
+        </div>
+        {provided.placeholder}
       </div>
-    </div>
-    <div className="Column__body">{children}</div>
-  </div>
+    )}
+  </Droppable>
 );
 
 Column.propTypes = {
--- app/assets/javascripts/components/projects/ProjectBoard.js
+++ app/assets/javascripts/components/projects/ProjectBoard.js
@@ -8,7 +8,7 @@ import Sprints from "../stories/Sprints";
 import History from "../stories/History";
 import { getColumns } from "../../selectors/columns";
 import * as Columns from '../../models/beta/column';
-import { createStory, closeHistory } from '../../actions/story';
+import { createStory, closeHistory, dragDropStory } from '../../actions/story';
 import AddStoryButton from '../story/AddStoryButton';
 import * as Story from 'libs/beta/constants';
 import PropTypes from 'prop-types';
@@ -19,10 +19,66 @@ import { removeNotification } from '../../actions/notifications';
 import StorySearch from '../search/StorySearch';
 import SearchResults from './../search/SearchResults';
 import ProjectLoading from './ProjectLoading';
+import { DragDropContext } from 'react-beautiful-dnd'
 
 export class ProjectBoard extends React.Component {
   componentWillMount() {
     this.props.fetchProjectBoard(this.props.projectId);
+  }
+
+  calculatePosition = (aboveStory, bellowStory) => {
+    if (bellowStory === undefined) return (Number(aboveStory.position) + 1)
+    if (aboveStory === undefined) return (Number(bellowStory.position) - 1);
+    return (Number(bellowStory.position) + Number(aboveStory.position)) / 2;
+  }
+
+  getNewPosition = (destinatitonIndex, sourceIndex, storiesArray, isSameColumn) => {
+    if (!isSameColumn) {
+      return this.calculatePosition(storiesArray[destinatitonIndex - 1], storiesArray[destinatitonIndex])
+    }
+    if (sourceIndex > destinatitonIndex) {
+      return this.calculatePosition(storiesArray[destinatitonIndex - 1], storiesArray[destinatitonIndex]);
+    }
+    return this.calculatePosition(storiesArray[destinatitonIndex], storiesArray[destinatitonIndex + 1]);
+  }
+
+  getArray = column => {
+    const { chillyBinStories, backlogSprints } = this.props;
+    return column === 'chillyBin' ? chillyBinStories : backlogSprints[0].stories;
+  }
+
+  getState = column => column === 'chillyBin' ? Story.status.UNSCHEDULED : Story.status.UNSTARTED
+
+  isSameColumn = (sourceColumn, destinationColumn) => sourceColumn === destinationColumn
+
+  onDragEnd = result => {
+    const { destination, source } = result;
+    const { dragDropStory } = this.props;
+    const destinationArray = this.getArray(destination.droppableId); // stories of destination column
+    const sourceArray = this.getArray(source.droppableId); // stories of source column
+    const isSameColumn = this.isSameColumn(source.droppableId, destination.droppableId);
+    const dragStory = sourceArray[source.index];
+
+    if (!destination) {
+      return;
+    }
+
+    if (isSameColumn && source.index === destination.index) {
+      return;
+    }
+
+    const newPosition = this.getNewPosition(destination.index, source.index, destinationArray, isSameColumn);
+
+    // Moving to same column
+    if (isSameColumn) {
+      dragDropStory(dragStory.id, dragStory.projectId, { position: newPosition });
+      return;
+    }
+
+    // Moving to a different column
+    const newState = this.getState(destination.droppableId);
+    dragDropStory(dragStory.id, dragStory.projectId, { position: newPosition, state: newState });
+    return;
   }
 
   render() {
@@ -30,76 +86,84 @@ export class ProjectBoard extends React.Component {
       return <ProjectLoading data-id="project-loading" />;
     }
 
-    const { 
-      projectId, 
-      createStory, 
+    const {
+      projectId,
+      createStory,
       closeHistory,
-      notifications, 
-      removeNotification, 
+      notifications,
+      removeNotification,
       history,
       projectBoard
     } = this.props;
 
     return (
-      <div className="ProjectBoard">
-        <StorySearch projectId={projectId} loading={projectBoard.search.loading} />
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="ProjectBoard">
+          <StorySearch projectId={projectId} loading={projectBoard.search.loading} />
 
-        <Notifications
-          notifications={notifications}
-          onRemove={removeNotification}
-        />
-
-        <Column title={I18n.t("projects.show.chilly_bin")}
-          renderAction={() =>
-            <AddStoryButton
-              onAdd={() => createStory({
-                state: Story.status.UNSCHEDULED
-              })}
-            />
-          }
-        >
-          <Stories stories={this.props.chillyBinStories} />
-        </Column>
-
-        <Column
-          title={`${I18n.t("projects.show.backlog")} /
-          ${I18n.t("projects.show.in_progress")}`}
-          renderAction={() =>
-            <AddStoryButton
-              onAdd={() => createStory({
-                state: Story.status.UNSTARTED
-              })}
-            />}
-        >
-          <Sprints
-            sprints={this.props.backlogSprints}
+          <Notifications
+            notifications={notifications}
+            onRemove={removeNotification}
           />
-        </Column>
 
-        <Column
-          title={I18n.t("projects.show.done")}
-        >
-          <Sprints
-            sprints={this.props.doneSprints}
-            fetchStories={this.props.fetchPastStories}
-          />
-        </Column>
-
-        <SearchResults />
-
-        {
-          history.status !== 'DISABLED' &&
-          <Column
-            onClose={closeHistory}
-            title={[I18n.t("projects.show.history"), "'", history.storyTitle, "'"].join(' ')}
-          >
-            { history.status === 'LOADED'
-              ? <History history={history.activities} />
-              : <div className="loading">Loading...</div>
+          <Column title={I18n.t("projects.show.chilly_bin")}
+            renderAction={() =>
+              <AddStoryButton
+                onAdd={() => createStory({
+                  state: Story.status.UNSCHEDULED
+                })}
+              />
             }
+            columnId={'chillyBin'}
+          >
+            <Stories stories={this.props.chillyBinStories} columnId={'chillyBin'} />
           </Column>
-        }
-      </div>
+
+          <Column
+            title={`${I18n.t("projects.show.backlog")} /
+          ${I18n.t("projects.show.in_progress")}`}
+            renderAction={() =>
+              <AddStoryButton
+                onAdd={() => createStory({
+                  state: Story.status.UNSTARTED
+                })}
+              />}
+            columnId={'backlog'}
+
+          >
+            <Sprints
+              sprints={this.props.backlogSprints}
+              columnId='backlog'
+            />
+          </Column>
+
+          <Column
+            title={I18n.t("projects.show.done")}
+            columnId={'done'}
+          >
+            <Sprints
+              sprints={this.props.doneSprints}
+              fetchStories={this.props.fetchPastStories}
+              columnId={'done'}
+            />
+          </Column>
+
+          <SearchResults />
+
+          {
+            history.status !== 'DISABLED' &&
+            <Column
+              onClose={closeHistory}
+              title={[I18n.t("projects.show.history"), "'", history.storyTitle, "'"].join(' ')}
+            >
+              {history.status === 'LOADED'
+                ? <History history={history.activities} />
+                : <div className="loading">Loading...</div>
+              }
+            </Column>
+          }
+        </div>
+      </DragDropContext>
     );
   }
 }
@@ -140,6 +204,7 @@ const mapStateToProps = ({
     pastIterations,
     stories
   }),
+  stories,
   notifications
 });
 
@@ -148,7 +213,8 @@ const mapDispatchToProps = {
   createStory,
   closeHistory,
   fetchPastStories,
-  removeNotification
+  removeNotification,
+  dragDropStory
 };
 
 export default connect(
